8850ac8e6c15937b6ab5cba4a5587c5beaa7816f
Nabin Krishan Chowdhury <nabinchowdhury26@gmail.com>
2025-07-27 21:44:50 +0600

    post API created

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import { AppDataSource } from "./data-source";
+import { User } from "./entity/User";
 import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 AppDataSource.initialize().then(async () => {
-    const userRepo = AppDataSource.getRepository(User)
+    const userRepo = AppDataSource.getRepository(User);
 
     app.get('/student', async (req, res) => {
-        const users = await userRepo.find()
-        res.send(users)
-    })
+        const users = await userRepo.find();
+        res.send(users);
+    });
+
+    app.post('/student', async (req, res) => {
+        const {firstName, lastName, age} = req.body;
+        const user = new User();
+        user.firstName = firstName;
+        user.lastName = lastName;
+        user.age = age;
+        
+        try {
+            const savedUser = await userRepo.save(user);
+            res.status(200).send(savedUser);
+        } catch (error) {
+            res.status(500).send(`Server error ${error}`);
+        }
+    });
 
     app.listen(port, () => {
-        console.log(`server running on http://localhost:${port}/`)
-    })
+        console.log(`server running on http://localhost:${port}/`);
+    });
 
     // console.log("Inserting a new user into the database...")
     // const user = new User() 
